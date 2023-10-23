700dadbd6284485253a18dbfc13a066b136fc8af
hiranur <145452148+hiranur13@users.noreply.github.com>
2023-10-23 14:35:26 +0300

    Add files via upload

---
+++ forornek2/forornek2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forornek2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int topla = 0;
+            int sayi1=Convert.ToInt32(textBox1.Text);
+            int sayi2=Convert.ToInt32(textBox2.Text);
+            for (int i = sayi1; i<=sayi2;++i)
+            {
+                topla += i;
+            }
+            label1.Text = topla.ToString();
+         
+        }
+    }
+}
